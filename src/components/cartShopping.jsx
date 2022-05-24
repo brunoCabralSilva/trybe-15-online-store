0e38590d680e5f31d3db2fd7c8799dba8f73bf36
TFlexa <thiago_flexa@hotmail.com>
2022-05-24 17:14:11 -0300

    Requisito 03 finalizado com lint

--- src/components/cartShopping.jsx
+++ src/components/cartShopping.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
 export default class cartShopping extends React.Component {
-    render () {
-        return (
-            <h1 data-testid="shopping-cart-empty-message">
-                Seu carrinho está vazio
-            </h1>
-        );
-    }
+  render() {
+    return (
+      <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+    );
+  }
 }
