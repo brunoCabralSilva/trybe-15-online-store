1abc5b8b87c74011351fae72ee263b2155fc1076
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-08-28 14:11:14 -0300

    Updates

---
+++ precache-manifest.0ee8c9ad71286d1ff59569f4b14cba29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99af524bc424c0f4573afa63e15374ec",
+    "url": "/online-storage/index.html"
+  },
+  {
+    "revision": "fa720a116481b425da1a",
+    "url": "/online-storage/static/css/main.b9d4f825.chunk.css"
+  },
+  {
+    "revision": "c2267b5d72b6b18eca0f",
+    "url": "/online-storage/static/js/2.02204141.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/online-storage/static/js/2.02204141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa720a116481b425da1a",
+    "url": "/online-storage/static/js/main.8fa8f979.chunk.js"
+  },
+  {
+    "revision": "e5c53753f71c43455f58",
+    "url": "/online-storage/static/js/runtime-main.4935b397.js"
+  },
+  {
+    "revision": "18cf3e9c519d73bc501736855c189442",
+    "url": "/online-storage/static/media/e-commerce.18cf3e9c.jpg"
+  },
+  {
+    "revision": "6d56361728353ed9b1cfe4393ec8511a",
+    "url": "/online-storage/static/media/trybe.6d563617.png"
+  }
+]);
